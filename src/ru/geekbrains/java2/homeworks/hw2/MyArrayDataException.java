ee1e6ea43a943d3bf3117320330f2c2db8e128d3
Munir <munirhasanov13@gmail.com>
2021-10-29 18:01:18 +0300

    домашнее задание 2

--- src/ru/geekbrains/java2/homeworks/hw2/MyArrayDataException.java
+++ src/ru/geekbrains/java2/homeworks/hw2/MyArrayDataException.java
@@ -1,4 +1,11 @@
 package ru.geekbrains.java2.homeworks.hw2;
 
-public class MyArrayDataException extends Exception {
+public class MyArrayDataException extends RuntimeException{
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
